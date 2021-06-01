f0d0e860b1f2ee1188c5a6547b801e471b8171b0
Jose Bezerra <jbqneto@gmail.com>
2021-05-31 22:34:07 -0300

    project start

---
+++ src/main/java/io/jbqneto/plantmanager/PlantmanagerApplication.java
@@ -0,0 +1,13 @@
+package io.jbqneto.plantmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/jbqneto/plantmanager/PlantmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.jbqneto.plantmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
